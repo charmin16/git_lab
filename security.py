60563a6cb4052e635db2994c156e590dd2858f69
Biokpo Princewill <prinzecharmin101@gmail.com>
2026-08-27 10:40:29 +0100

    write password hash

--- security.py
+++ security.py
@@ -8,3 +8,16 @@ from datetime import datetime, date, timedelta
 
 password_hasher = PasswordHash.recommended()
 oauth_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+
+# create hashed password
+def hash_password(password: str):
+    return password_hasher.hash(password)
+
+
+# verify hashed password
+def verify_hashed_pwd(hashed: str, plain: str):
+    return password_hasher.verify(plain, hashed)
+
+
+
