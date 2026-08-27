d30a56679b281ca75662325da44bc98bfd0f2d10
Biokpo Princewill <prinzecharmin101@gmail.com>
2026-08-27 10:48:58 +0100

    create access token

--- security.py
+++ security.py
@@ -5,9 +5,17 @@ from database import get_db
 import jwt
 from pwdlib import PasswordHash
 from datetime import datetime, date, timedelta
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 password_hasher = PasswordHash.recommended()
 oauth_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+ACCESS_TOKEN_EXPIRY = 400
+ALGORITHM = "HS256"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 
 # create hashed password
@@ -18,6 +26,23 @@ def hash_password(password: str):
 # verify hashed password
 def verify_hashed_pwd(hashed: str, plain: str):
     return password_hasher.verify(plain, hashed)
+
+
+# create token
+def create_token(data: dict):
+    to_encode = data.copy()
+
+    to_encode.update({"expiry": datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRY)})
+
+    jwt_encoded = jwt.encode(
+        to_encode,
+        SECRET_KEY,
+        ALGORITHM=ALGORITHM
+    )
+
+    return jwt_encoded
+
+
 
 
 
