02bb1474a9b1c10a2b210e7cc1adb37a0f46321d
Biokpo Princewill <prinzecharmin101@gmail.com>
2026-08-29 02:24:50 +0100

    add item model to models.py

--- models.py
+++ models.py
@@ -13,12 +13,13 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     username = Column(String(100), nullable=False)
     password_hash = Column(String(255), nullable=False)
+    phone = Column(String(12), nullable=False)
     display_name = Column(String(100), nullable=False)
     role = Column(SQLEnum(UserRole), default=UserRole.USER)
     created_at = Column(Date, default=date.today)
 
     properties = relationship(
-        "User",
+        "Property",
         back_populates="user"
     )
 
@@ -29,19 +30,19 @@ class Property(Base):
     title = Column(String(100), nullable=False)
     city = Column(String(100), nullable=False)
     area = Column(String(100), nullable=False)
-    bedrooms = Column(Integer, nullable=False)
+    bedrooms = Column(Integer, nullable=True)
     listing = Column(SQLEnum(ListingType))
-    status = Column(SQLEnum(PropertyStatus), default=PropertyStatus.PENDING)
-    price = Column(Numeric(3, 2), nullable=False)
-    annual_rent = Column(Numeric(3, 2), nullable=False)
-    lawyer_fee = Column(Numeric(3, 2), nullable=False)
-    caution_fee = Column(Numeric(3, 2), nullable=False)
-    has_c_of_o = Column(Boolean, nullable=False)
+    status = Column(SQLEnum(PropertyStatus), default=PropertyStatus.APPROVED)
+    price = Column(Numeric(3, 2), nullable=True)
+    annual_rent = Column(Numeric(3, 2), nullable=True)
+    lawyer_fee = Column(Numeric(3, 2), nullable=True)
+    caution_fee = Column(Numeric(3, 2), nullable=True)
+    has_c_of_o = Column(Boolean, nullable=True)
     created_at = Column(DateTime, default=datetime.utcnow)
 
     user_id = Column(Integer, ForeignKey("user.id", ondelete="CASCADE"))
 
-    users = relationship(
+    user = relationship(
         "User",
         back_populates="properties"
     )
@@ -55,15 +56,21 @@ class Property(Base):
 class PropertyMedia(Base):
     __tablename__ = "property_media"
     id = Column(Integer, primary_key=True)
-    filepath = Column(String(200), nullable=True)
     filename = Column(String(200), nullable=True)
+    filepath = Column(String(200), nullable=True)
     media_type = Column(String(50), nullable=True)
 
-    property_id = Column(Integer, ForeignKey("property.id", nullable=False))
+    property_id = Column(Integer, ForeignKey("property.id"))
 
     property = relationship(
         "Property",
         back_populates="prop_media"
     )
+
+
+class Item(Base):
+    __tablename__ = "item"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
 
 
